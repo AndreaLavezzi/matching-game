9e5ca30167e12c92204022dae6ae9cc44134c23e
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2021-10-13 19:11:18 +0200

    Non ricordo cos'ho modificato aiut

--- matchingGame/Form1.Designer.cs
+++ matchingGame/Form1.Designer.cs
@@ -75,15 +75,16 @@ namespace matchingGame
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Location = new System.Drawing.Point(118, 12);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
@@ -92,6 +93,7 @@ namespace matchingGame
             this.pictureBox3.Location = new System.Drawing.Point(224, 12);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
@@ -100,6 +102,7 @@ namespace matchingGame
             this.pictureBox4.Location = new System.Drawing.Point(330, 12);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
             // 
@@ -108,6 +111,7 @@ namespace matchingGame
             this.pictureBox5.Location = new System.Drawing.Point(436, 12);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
             // 
@@ -116,6 +120,7 @@ namespace matchingGame
             this.pictureBox6.Location = new System.Drawing.Point(542, 12);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 5;
             this.pictureBox6.TabStop = false;
             // 
@@ -124,6 +129,7 @@ namespace matchingGame
             this.pictureBox7.Location = new System.Drawing.Point(12, 118);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox7.TabIndex = 6;
             this.pictureBox7.TabStop = false;
             // 
@@ -132,6 +138,7 @@ namespace matchingGame
             this.pictureBox8.Location = new System.Drawing.Point(118, 118);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox8.TabIndex = 7;
             this.pictureBox8.TabStop = false;
             // 
@@ -140,6 +147,7 @@ namespace matchingGame
             this.pictureBox9.Location = new System.Drawing.Point(224, 118);
             this.pictureBox9.Name = "pictureBox9";
             this.pictureBox9.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox9.TabIndex = 8;
             this.pictureBox9.TabStop = false;
             // 
@@ -148,6 +156,7 @@ namespace matchingGame
             this.pictureBox10.Location = new System.Drawing.Point(330, 118);
             this.pictureBox10.Name = "pictureBox10";
             this.pictureBox10.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox10.TabIndex = 9;
             this.pictureBox10.TabStop = false;
             // 
@@ -156,6 +165,7 @@ namespace matchingGame
             this.pictureBox11.Location = new System.Drawing.Point(436, 118);
             this.pictureBox11.Name = "pictureBox11";
             this.pictureBox11.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox11.TabIndex = 10;
             this.pictureBox11.TabStop = false;
             // 
@@ -164,6 +174,7 @@ namespace matchingGame
             this.pictureBox12.Location = new System.Drawing.Point(542, 118);
             this.pictureBox12.Name = "pictureBox12";
             this.pictureBox12.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox12.TabIndex = 11;
             this.pictureBox12.TabStop = false;
             // 
@@ -172,6 +183,7 @@ namespace matchingGame
             this.pictureBox13.Location = new System.Drawing.Point(12, 224);
             this.pictureBox13.Name = "pictureBox13";
             this.pictureBox13.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox13.TabIndex = 12;
             this.pictureBox13.TabStop = false;
             // 
@@ -180,6 +192,7 @@ namespace matchingGame
             this.pictureBox14.Location = new System.Drawing.Point(118, 224);
             this.pictureBox14.Name = "pictureBox14";
             this.pictureBox14.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox14.TabIndex = 13;
             this.pictureBox14.TabStop = false;
             // 
@@ -188,6 +201,7 @@ namespace matchingGame
             this.pictureBox15.Location = new System.Drawing.Point(224, 224);
             this.pictureBox15.Name = "pictureBox15";
             this.pictureBox15.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox15.TabIndex = 14;
             this.pictureBox15.TabStop = false;
             // 
@@ -196,6 +210,7 @@ namespace matchingGame
             this.pictureBox16.Location = new System.Drawing.Point(330, 224);
             this.pictureBox16.Name = "pictureBox16";
             this.pictureBox16.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox16.TabIndex = 15;
             this.pictureBox16.TabStop = false;
             // 
@@ -204,6 +219,7 @@ namespace matchingGame
             this.pictureBox17.Location = new System.Drawing.Point(436, 224);
             this.pictureBox17.Name = "pictureBox17";
             this.pictureBox17.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox17.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox17.TabIndex = 16;
             this.pictureBox17.TabStop = false;
             // 
@@ -212,6 +228,7 @@ namespace matchingGame
             this.pictureBox18.Location = new System.Drawing.Point(542, 224);
             this.pictureBox18.Name = "pictureBox18";
             this.pictureBox18.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox18.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox18.TabIndex = 17;
             this.pictureBox18.TabStop = false;
             // 
@@ -268,6 +285,7 @@ namespace matchingGame
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- matchingGame/Form1.cs
+++ matchingGame/Form1.cs
@@ -10,8 +10,13 @@ using System.Windows.Forms;
 
 namespace matchingGame
 {
+    
     public partial class Form1 : Form
     {
+        PictureBox[] pictureBoxesList = new PictureBox[18];
+        PictureBox card1, card2;
+        bool isFirstCard = true;
+        
         public Form1()
         {
             InitializeComponent();
@@ -19,16 +24,52 @@ namespace matchingGame
 
         void LoadImages()
         {
-            int[] imagesIndexes = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
             PictureBox[] pictureBoxes = { pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6, pictureBox7, pictureBox8, pictureBox9, pictureBox10, pictureBox11, pictureBox12, pictureBox13, pictureBox14, pictureBox15, pictureBox16, pictureBox17, pictureBox18 };
+            for(int i = 0; i < pictureBoxes.Length; i++)
+            {
+                pictureBoxesList[i] = pictureBoxes[i];
+            }
+            int[] imagesIndexes = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
+            
             Random random = new Random();
             imagesIndexes = imagesIndexes.OrderBy(x => random.Next()).ToArray();
-            bool doubleImage = false;
+
             int j = 1;
-            for(int i = 0; i<18; i++)
+            for(int i = 0; i < pictureBoxesList.Length; i++)
+            {                
+                pictureBoxesList[i].Image = Image.FromFile(@"images/0.jpg");
+                pictureBoxesList[i].Name = imagesIndexes[i].ToString() + ".jpg";
+                pictureBoxesList[i].Click += new EventHandler(CheckMatch);
+            }
+        }
+
+        void CheckMatch(object sender, EventArgs e)
+        {
+            PictureBox clickedCard = sender as PictureBox;
+            if(card1 != null || card2 != null)
             {
-                
-                pictureBoxes[i].Image = Image.FromFile(@"images/" + imagesIndexes[i].ToString() + ".jpg");
+                if (isFirstCard)
+                {
+                    card1 = clickedCard;
+                    card1.Image = Image.FromFile(@"images/" + card1.Name);
+                    isFirstCard = false;
+                }
+                else
+                {
+                    card2 = clickedCard;
+                    if (card1.Name == card2.Name && card1.Location != card2.Location)
+                    {
+                        MessageBox.Show("BROOO");
+                    }
+                    else
+                    {
+                        card1.Image = Image.FromFile(@"images/0.jpg");
+                        card2.Image = Image.FromFile(@"images/0.jpg");
+                        card1 = null;
+                        card2 = null;
+                    }
+                    isFirstCard = true;
+                }
             }
         }
 
@@ -37,9 +78,27 @@ namespace matchingGame
             LoadImages();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        public void wait(int milliseconds)
         {
-            MessageBox.Show("hai cliccato su pic1");
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            // Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Console.WriteLine("stop wait timer");
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
         }
     }
 }
